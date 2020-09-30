756263c8babeb0c8a4dec9ce2aefe124f67ca0ea
Ali Mortazavi <ali.mortazavi121@gmail.com>
2020-09-30 15:30:36 +0330

    delete and update post added

--- controllers/admin/post.js
+++ controllers/admin/post.js
@@ -24,6 +24,11 @@ exports.savePost = async (req, res, next) => {
         const title = req.body.title;
         const content = req.body.content;
         const errors = validationResult(req);
+        const body = {
+            title,
+            content,
+            userId: req.user._id
+        };
         if(!errors.isEmpty()) {
             req.session.errorsMessage = errors.array();
             req.session.formData = {
@@ -32,12 +37,46 @@ exports.savePost = async (req, res, next) => {
             };
             return res.redirect('/admin/posts/add');
         }
-        const post = new Post({
-            title,
-            content,
-            userId: req.user._id
-        });
+        const post = new Post(body);
         await post.save();
+        res.redirect('/admin/posts')
+    } catch(err) {
+        console.log('err', err);
+    }
+};
+
+exports.editPost = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const data = await Post.findById(id).populate('userId').exec();
+        res.render('admin/posts/form', {
+            headTitle: 'Edit Post',
+            formData: data
+        });
+    } catch(err) {
+        console.log('err', err);
+    }
+};
+exports.updatePost = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const title = req.body.title;
+        const content = req.body.content;
+        const body = {
+            title,
+            content
+        };
+        await Post.findOneAndUpdate(id, body);
+        res.redirect('/admin/posts')
+    } catch(err) {
+        console.log('err', err);
+    }
+};
+
+exports.deletePost = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        await Post.findByIdAndDelete(id);
         res.redirect('/admin/posts')
     } catch(err) {
         console.log('err', err);
--- routes/admin/post.js
+++ routes/admin/post.js
@@ -6,5 +6,8 @@ const adminPostController = require('../../controllers/admin/post');
 router.get('/', adminPostController.getPosts);
 router.get('/add', adminPostController.addPost);
 router.post('/add', postValidation, adminPostController.savePost);
+router.get('/:id/edit', adminPostController.editPost);
+router.post('/:id/edit', adminPostController.updatePost);
+router.get('/:id/delete', adminPostController.deletePost);
 
 module.exports = router;
