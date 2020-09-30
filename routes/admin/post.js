135bd237a511fc2ca569fbaf880eee092385c7ff
Ali Mortazavi <ali.mortazavi121@gmail.com>
2020-09-30 15:30:43 +0330

    delete and update post added

--- routes/admin/post.js
+++ routes/admin/post.js
@@ -7,7 +7,7 @@ router.get('/', adminPostController.getPosts);
 router.get('/add', adminPostController.addPost);
 router.post('/add', postValidation, adminPostController.savePost);
 router.get('/:id/edit', adminPostController.editPost);
-router.post('/:id/edit', adminPostController.updatePost);
+router.post('/:id/edit', postValidation, adminPostController.updatePost);
 router.get('/:id/delete', adminPostController.deletePost);
 
 module.exports = router;
